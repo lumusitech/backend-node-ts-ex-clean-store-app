27228804e6d664893c26536169fb6309f4632dee
luciano <lumusitech@gmail.com>
2024-03-15 14:53:01 -0300

    feat: update validateToken to receive generics

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -18,12 +18,12 @@ export class JWTAdapter {
     })
   }
 
-  static validateToken(token: string) {
+  static validateToken<T>(token: string): Promise<T | null> {
     return new Promise(resolve => {
       jwt.verify(token, JWT_SEED, (err, decoded) => {
         if (err) resolve(null)
 
-        resolve(decoded)
+        resolve(decoded as T)
       })
     })
   }
