4cba5b461515081167a15a22756359bc20f06b99
luciano <lumusitech@gmail.com>
2024-03-19 14:27:22 -0300

    feat: add categories dto

---
+++ src/domain/dtos/shared/pagination.dto.ts
@@ -0,0 +1,12 @@
+export class PaginationDTO {
+  private constructor(public readonly page: number, public readonly limit: number) {}
+
+  static create(page: number, limit: number): [string?, PaginationDTO?] {
+    if (isNaN(page) || isNaN(limit)) return ['Page and limit must be numbers']
+
+    if (page <= 0) return ['Page must be greater than 0']
+    if (limit <= 0) return ['Limit must be greater than 0']
+
+    return [undefined, new PaginationDTO(page, limit)]
+  }
+}
